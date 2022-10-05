2f5e512df294ccaacb006686eaaa6f8a57caec70
Azer444 <azerrh@code.edu.az>
2022-10-05 22:42:59 +0400

    Front-To-Back

---
+++ Front-To-Back/Front-To-Back/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Front_To_Back.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Front_To_Back.Controllers
+{
+    public class HomeController : Controller
+    {
+        Car car = new Car
+        {
+            Id = 150,
+            Brand = "Mercedes"
+        };
+
+        public IActionResult Index()
+        {
+            return View(car);
+        }
+
+    }
+}
